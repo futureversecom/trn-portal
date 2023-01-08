693a52e965e66d1d9f5a49ebf35fc64c8c903118
aidan-starke <starke.aidan@gmail.com>
2023-01-09 10:22:55 +1300

    Clean up endpoints

--- packages/apps-config/src/endpoints/index.ts
+++ packages/apps-config/src/endpoints/index.ts
@@ -5,9 +5,9 @@ import type { TFunction } from '../types';
 import type { LinkOption } from './types';
 
 import { defaultT } from '../util';
-import { createCustom, createDev, createOwn } from './development';
-import { prodChains, prodRelayKusama, prodRelayPolkadot } from './production';
-import { testChains, testRelayRococo, testRelayWestend } from './testing';
+import { createDev, createOwn } from './development';
+import { prodChains } from './production';
+import { testChains } from './testing';
 import { expandEndpoints } from './util';
 
 export { CUSTOM_ENDPOINT_KEY } from './development';
@@ -16,41 +16,6 @@ export * from './testing';
 
 export function createWsEndpoints (t: TFunction = defaultT, firstOnly = false, withSort = true): LinkOption[] {
   return [
-    ...createCustom(t),
-    {
-      isDisabled: false,
-      isHeader: true,
-      isSpaced: true,
-      text: t('rpc.header.polkadot.relay', 'Polkadot & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...expandEndpoints(t, [prodRelayPolkadot], firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      text: t('rpc.header.kusama.relay', 'Kusama & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...expandEndpoints(t, [prodRelayKusama], firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      isSpaced: true,
-      text: t('rpc.header.westend.relay', 'Test Westend & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...expandEndpoints(t, [testRelayWestend], firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      text: t('rpc.header.rococo.relay', 'Test Rococo & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...expandEndpoints(t, [testRelayRococo], firstOnly, withSort),
     {
       isDisabled: false,
       isHeader: true,
