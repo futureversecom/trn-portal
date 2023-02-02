0f2149011c037e9a32db2cb23654e5f14602929e
KarishmaBothara <bothara.karishma@gmail.com>
2023-02-02 14:34:37 +0400

    Fix MetaMask part

--- packages/react-signer/src/TxMetaMaskSign.tsx
+++ packages/react-signer/src/TxMetaMaskSign.tsx
@@ -8,7 +8,7 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { Button, ErrorBoundary, Modal, Output, StatusContext } from '@polkadot/react-components';
-import { useApi, useMetaMask, useToggle } from '@polkadot/react-hooks';
+import {useApi, useMetaMask, useQueue, useToggle} from '@polkadot/react-hooks';
 import { nextTick } from '@polkadot/util';
 
 import Address from './Address';
@@ -37,7 +37,7 @@ const EMPTY_INNER: InnerTx = { innerHash: null, innerTx: null };
 function TxSigned ({ className, currentItem, requestAddress }: Props): React.ReactElement<Props> | null {
   const { t } = useTranslation();
   const { api } = useApi();
-  const { queueSetTxStatus } = useContext(StatusContext);
+  const { queueSetTxStatus } = useQueue();
   const [error] = useState<Error | null>(null);
   const [passwordError, setPasswordError] = useState<string | null>(null);
   const [isBusy, setBusy] = useState(false);
