dc4c3ba4125ee6dd2104d0fe37399c766022cf8b
KarishmaBothara <bothara.karishma@gmail.com>
2023-02-28 12:13:56 +0400

    Fix lint

--- packages/page-staking/src/Actions/Account/InputValidationController.tsx
+++ packages/page-staking/src/Actions/Account/InputValidationController.tsx
@@ -7,10 +7,9 @@ import type { AccountId, StakingLedger } from '@polkadot/types/interfaces';
 
 import React, { useEffect, useState } from 'react';
 
-import { PalletAssetsAssetAccount } from '@polkadot/types/lookup';
-
 import { MarkError, MarkWarning } from '@polkadot/react-components';
 import { useApi, useCall } from '@polkadot/react-hooks';
+import { PalletAssetsAssetAccount } from '@polkadot/types/lookup';
 
 import { useTranslation } from '../../translate';
 
