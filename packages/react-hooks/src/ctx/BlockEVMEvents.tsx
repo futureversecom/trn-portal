3e8d40512fa2694cf58d51803fb11517a0263cbe
KarishmaBothara <bothara.karishma@gmail.com>
2023-03-20 13:37:51 +0400

    Fix lint

--- packages/react-hooks/src/ctx/BlockEVMEvents.tsx
+++ packages/react-hooks/src/ctx/BlockEVMEvents.tsx
@@ -1,7 +1,6 @@
 // Copyright 2017-2023 @polkadot/react-query authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { ApiPromise } from '@polkadot/api';
 import type { Vec } from '@polkadot/types';
 import type { EthTransactionStatus } from '@polkadot/types/interfaces';
 
@@ -26,7 +25,7 @@ const DEFAULT_EVENTS: BlockEVMEvents = { evmEventCount: 0, evmEvents: [] };
 
 export const BlockEVMEventsCtx = React.createContext<BlockEVMEvents>(DEFAULT_EVENTS);
 
-function manageEvents (api: ApiPromise, prev: PrevHashes, eventsRecords: Vec<EthTransactionStatus>, setState: React.Dispatch<React.SetStateAction<BlockEVMEvents>>): void {
+function manageEvents (prev: PrevHashes, eventsRecords: Vec<EthTransactionStatus>, setState: React.Dispatch<React.SetStateAction<BlockEVMEvents>>): void {
   if (eventsRecords.length) {
     setState(() => ({
       evmEventCount: eventsRecords.length,
@@ -42,13 +41,16 @@ export function BlockEVMEventsCtxRoot ({ children }: Props): React.ReactElement<
 
   const prevHashes = useRef({ block: null, event: null, txHash: null });
   const events = records?.unwrap().toJSON() as unknown as Vec<EthTransactionStatus>;
-  const transactionHashes = events ? new Set(events.map((evt) => evt.transactionHash)) : new Set();
+  const stateEvnts = state.evmEvents;
 
   useEffect((): void => {
-    const mergedEvents = (events ? [...events, ...state.evmEvents?.filter(e => !transactionHashes.has(e.transactionHash))] :
-      state.evmEvents) as unknown as Vec<EthTransactionStatus>;
-    records && manageEvents(api, prevHashes.current, mergedEvents, setState);
-  }, [api, prevHashes, records, setState]);
+    const transactionHashes = events ? new Set(events.map((evt) => evt.transactionHash)) : new Set();
+    const mergedEvents = (events && stateEvnts
+      ? [...events, ...stateEvnts.filter((e) => !transactionHashes.has(e.transactionHash))]
+      : stateEvnts) as unknown as Vec<EthTransactionStatus>;
+
+    records && manageEvents(prevHashes.current, mergedEvents, setState);
+  }, [prevHashes, records, setState, events, stateEvnts]);
 
   return (
     <BlockEVMEventsCtx.Provider value={state}>
