2e638c877c23ccdadefb9d97e833e32a724d3863
KarishmaBothara <bothara.karishma@gmail.com>
2023-03-28 11:59:38 +0530

    Added recent blocks thing

--- packages/page-explorer/src/EVMEvents.tsx
+++ packages/page-explorer/src/EVMEvents.tsx
@@ -4,7 +4,11 @@
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 
-import { MarkError, Table } from '@polkadot/react-components';
+import BlockHeaders from '@polkadot/app-explorer/BlockHeaders';
+import Query from '@polkadot/app-explorer/Query';
+import Summary from '@polkadot/app-explorer/Summary';
+import { Columar, MarkError, Table } from '@polkadot/react-components';
+import { useBlockAuthors } from '@polkadot/react-hooks';
 import { BlockEVMEvents } from '@polkadot/react-hooks/ctx/types';
 
 import EVMEvent from './EVMEvent';
@@ -48,25 +52,38 @@ function EVMEvents ({ className = '', emptyLabel, error, eventClassName, events,
     ],
     [label, t]
   );
+  const { lastHeaders } = useBlockAuthors();
 
   return (
-    <Table
-      className={className}
-      empty={emptyLabel || t<string>('No events available')}
-      header={header}
-    >
-      {error
-        ? (
-          <tr
-            className={eventClassName}
-            key='error'
+    <>
+      <Query />
+      <Summary eventCount={0} />
+      <Columar>
+        <Columar.Column>
+          <BlockHeaders headers={lastHeaders} />
+        </Columar.Column>
+        <Columar.Column>
+
+          <Table
+            className={className}
+            empty={emptyLabel || t<string>('No events available')}
+            header={header}
           >
-            <td><MarkError content={t<string>('Unable to decode the block events. {{error}}', { replace: { error: error.message } })} /></td>
-          </tr>
-        )
-        : events && events.map((e) => renderEvent(eventClassName, e))
-      }
-    </Table>
+            {error
+              ? (
+                <tr
+                  className={eventClassName}
+                  key='error'
+                >
+                  <td><MarkError content={t<string>('Unable to decode the block events. {{error}}', { replace: { error: error.message } })} /></td>
+                </tr>
+              )
+              : events && events.map((e) => renderEvent(eventClassName, e))
+            }
+          </Table>
+        </Columar.Column>
+      </Columar>
+    </>
   );
 }
 
