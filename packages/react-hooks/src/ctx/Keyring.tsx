308528fa119d7239f61d9c4e6a74e76e69c81d30
KarishmaBothara <bothara.karishma@gmail.com>
2024-06-04 20:56:57 +0400

    Fix vercel build

--- packages/react-hooks/src/ctx/Keyring.tsx
+++ packages/react-hooks/src/ctx/Keyring.tsx
@@ -11,7 +11,7 @@ import { keyring } from '@polkadot/ui-keyring';
 import { u8aToHex } from '@polkadot/util';
 import { decodeAddress } from '@polkadot/util-crypto';
 
-import { useApi } from '../useApi.js';
+import { useApi } from '../useApi';
 
 interface Props {
   children?: React.ReactNode;
