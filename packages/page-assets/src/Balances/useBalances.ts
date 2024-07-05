9520eace732450664853c4eae582023344ffd1a4
KarishmaBothara <bothara.karishma@gmail.com>
2024-07-05 11:28:22 +0530

    Ignore ts lint

--- packages/page-assets/src/Balances/useBalances.ts
+++ packages/page-assets/src/Balances/useBalances.ts
@@ -55,6 +55,8 @@ const BALANCES_OPTS = {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       const { data: { feeFrozen, free, frozen, miscFrozen, reserved } } = account;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       const balance = (specVersion as number) < 55 ? free.sub(BN.max(feeFrozen, miscFrozen)) : (free.add(reserved)).sub(frozen);
 
       return {
